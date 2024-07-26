3f59fcbd8fc02ad822e032b231cef67c31f8d300
Psolite <info@bulldevelopers.com>
2024-07-26 16:35:16 +0200

    Calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// Calculation with rust
+// fn main (){
+//     let a: f64 = 66.0;
+//     let b: f64 = 9.0;
+
+//     let result = a / b;
+//     println!(" The sum of the number is {}", result)
+// }
+
+// the result show have decimal, so if you are expecting decimal you will have to use f64
+// you have to make the values decimal too
+
+// What happen when you try to buy something with you card
+
 fn main() {
-    println!("Hello, I am new to rust");
+    let balance: f64 = 59.0;
+    let shoes: f64 = 58.1;
+    let bonus: f64 = 0.9;
+
+    let result = balance - (shoes + bonus); // BODMAS know will help you with this 
+    if balance > shoes  {
+        println!("you have paid and your balance is {}", result);
+    } else {
+        println!("You are broke");
+    }
 }
+
